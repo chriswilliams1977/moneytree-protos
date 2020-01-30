01094b97988b9797d1a40867ec3dfc1c3302b36e
chriswilliams1977 <chriswilliams1977@gmail.com>
2020-01-30 11:26:01 +0100

    updating protos

--- account/account.pb.go
+++ account/account.pb.go
@@ -89,46 +89,46 @@ func (m *Account) GetBalance() int32 {
 	return 0
 }
 
-type Request struct {
+type AccountRequest struct {
 	AccountNumber        string   `protobuf:"bytes,1,opt,name=account_number,json=accountNumber,proto3" json:"account_number,omitempty"`
 	XXX_NoUnkeyedLiteral struct{} `json:"-"`
 	XXX_unrecognized     []byte   `json:"-"`
 	XXX_sizecache        int32    `json:"-"`
 }
 
-func (m *Request) Reset()         { *m = Request{} }
-func (m *Request) String() string { return proto.CompactTextString(m) }
-func (*Request) ProtoMessage()    {}
-func (*Request) Descriptor() ([]byte, []int) {
+func (m *AccountRequest) Reset()         { *m = AccountRequest{} }
+func (m *AccountRequest) String() string { return proto.CompactTextString(m) }
+func (*AccountRequest) ProtoMessage()    {}
+func (*AccountRequest) Descriptor() ([]byte, []int) {
 	return fileDescriptor_8e28828dcb8d24f0, []int{1}
 }
 
-func (m *Request) XXX_Unmarshal(b []byte) error {
-	return xxx_messageInfo_Request.Unmarshal(m, b)
+func (m *AccountRequest) XXX_Unmarshal(b []byte) error {
+	return xxx_messageInfo_AccountRequest.Unmarshal(m, b)
 }
-func (m *Request) XXX_Marshal(b []byte, deterministic bool) ([]byte, error) {
-	return xxx_messageInfo_Request.Marshal(b, m, deterministic)
+func (m *AccountRequest) XXX_Marshal(b []byte, deterministic bool) ([]byte, error) {
+	return xxx_messageInfo_AccountRequest.Marshal(b, m, deterministic)
 }
-func (m *Request) XXX_Merge(src proto.Message) {
-	xxx_messageInfo_Request.Merge(m, src)
+func (m *AccountRequest) XXX_Merge(src proto.Message) {
+	xxx_messageInfo_AccountRequest.Merge(m, src)
 }
-func (m *Request) XXX_Size() int {
-	return xxx_messageInfo_Request.Size(m)
+func (m *AccountRequest) XXX_Size() int {
+	return xxx_messageInfo_AccountRequest.Size(m)
 }
-func (m *Request) XXX_DiscardUnknown() {
-	xxx_messageInfo_Request.DiscardUnknown(m)
+func (m *AccountRequest) XXX_DiscardUnknown() {
+	xxx_messageInfo_AccountRequest.DiscardUnknown(m)
 }
 
-var xxx_messageInfo_Request proto.InternalMessageInfo
+var xxx_messageInfo_AccountRequest proto.InternalMessageInfo
 
-func (m *Request) GetAccountNumber() string {
+func (m *AccountRequest) GetAccountNumber() string {
 	if m != nil {
 		return m.AccountNumber
 	}
 	return ""
 }
 
-type Response struct {
+type AccountResponse struct {
 	Account              *Account   `protobuf:"bytes,1,opt,name=account,proto3" json:"account,omitempty"`
 	Accounts             []*Account `protobuf:"bytes,2,rep,name=accounts,proto3" json:"accounts,omitempty"`
 	XXX_NoUnkeyedLiteral struct{}   `json:"-"`
@@ -136,39 +136,39 @@ type Response struct {
 	XXX_sizecache        int32      `json:"-"`
 }
 
-func (m *Response) Reset()         { *m = Response{} }
-func (m *Response) String() string { return proto.CompactTextString(m) }
-func (*Response) ProtoMessage()    {}
-func (*Response) Descriptor() ([]byte, []int) {
+func (m *AccountResponse) Reset()         { *m = AccountResponse{} }
+func (m *AccountResponse) String() string { return proto.CompactTextString(m) }
+func (*AccountResponse) ProtoMessage()    {}
+func (*AccountResponse) Descriptor() ([]byte, []int) {
 	return fileDescriptor_8e28828dcb8d24f0, []int{2}
 }
 
-func (m *Response) XXX_Unmarshal(b []byte) error {
-	return xxx_messageInfo_Response.Unmarshal(m, b)
+func (m *AccountResponse) XXX_Unmarshal(b []byte) error {
+	return xxx_messageInfo_AccountResponse.Unmarshal(m, b)
 }
-func (m *Response) XXX_Marshal(b []byte, deterministic bool) ([]byte, error) {
-	return xxx_messageInfo_Response.Marshal(b, m, deterministic)
+func (m *AccountResponse) XXX_Marshal(b []byte, deterministic bool) ([]byte, error) {
+	return xxx_messageInfo_AccountResponse.Marshal(b, m, deterministic)
 }
-func (m *Response) XXX_Merge(src proto.Message) {
-	xxx_messageInfo_Response.Merge(m, src)
+func (m *AccountResponse) XXX_Merge(src proto.Message) {
+	xxx_messageInfo_AccountResponse.Merge(m, src)
 }
-func (m *Response) XXX_Size() int {
-	return xxx_messageInfo_Response.Size(m)
+func (m *AccountResponse) XXX_Size() int {
+	return xxx_messageInfo_AccountResponse.Size(m)
 }
-func (m *Response) XXX_DiscardUnknown() {
-	xxx_messageInfo_Response.DiscardUnknown(m)
+func (m *AccountResponse) XXX_DiscardUnknown() {
+	xxx_messageInfo_AccountResponse.DiscardUnknown(m)
 }
 
-var xxx_messageInfo_Response proto.InternalMessageInfo
+var xxx_messageInfo_AccountResponse proto.InternalMessageInfo
 
-func (m *Response) GetAccount() *Account {
+func (m *AccountResponse) GetAccount() *Account {
 	if m != nil {
 		return m.Account
 	}
 	return nil
 }
 
-func (m *Response) GetAccounts() []*Account {
+func (m *AccountResponse) GetAccounts() []*Account {
 	if m != nil {
 		return m.Accounts
 	}
@@ -176,33 +176,33 @@ func (m *Response) GetAccounts() []*Account {
 }
 
 func init() {
-	proto.RegisterType((*Account)(nil), "moneytree.svc.account.Account")
-	proto.RegisterType((*Request)(nil), "moneytree.svc.account.Request")
-	proto.RegisterType((*Response)(nil), "moneytree.svc.account.Response")
+	proto.RegisterType((*Account)(nil), "moneytree.svc.proto.Account")
+	proto.RegisterType((*AccountRequest)(nil), "moneytree.svc.proto.AccountRequest")
+	proto.RegisterType((*AccountResponse)(nil), "moneytree.svc.proto.AccountResponse")
 }
 
 func init() { proto.RegisterFile("account.proto", fileDescriptor_8e28828dcb8d24f0) }
 
 var fileDescriptor_8e28828dcb8d24f0 = []byte{
-	// 279 bytes of a gzipped FileDescriptorProto
-	0x1f, 0x8b, 0x08, 0x00, 0x00, 0x00, 0x00, 0x00, 0x02, 0xff, 0xac, 0x52, 0x41, 0x4b, 0x33, 0x31,
-	0x10, 0xfd, 0x76, 0xdb, 0xaf, 0xdb, 0x4e, 0x69, 0x0f, 0x03, 0x4a, 0xf0, 0xa0, 0xcb, 0x82, 0xd0,
-	0x53, 0x90, 0x7a, 0x11, 0x6f, 0xd6, 0x83, 0x37, 0x91, 0x78, 0x10, 0xbc, 0x48, 0x36, 0x0c, 0x22,
-	0xd8, 0x64, 0x4d, 0xb2, 0x85, 0xbd, 0xf9, 0x0f, 0xfc, 0xcb, 0x62, 0x36, 0x51, 0x04, 0x8b, 0x3d,
-	0x78, 0x9b, 0x99, 0xf7, 0xe6, 0x4d, 0xde, 0x23, 0x30, 0x93, 0x4a, 0x99, 0x56, 0x7b, 0xde, 0x58,
-	0xe3, 0x0d, 0xee, 0xad, 0x8d, 0xa6, 0xce, 0x5b, 0x22, 0xee, 0x36, 0x8a, 0x47, 0xb0, 0x7a, 0x84,
-	0xe2, 0xa2, 0x2f, 0x71, 0x1f, 0x46, 0xba, 0x5d, 0xd7, 0x64, 0x59, 0x56, 0x66, 0x8b, 0x89, 0x88,
-	0x1d, 0x22, 0x0c, 0x7d, 0xd7, 0x10, 0xcb, 0xc3, 0x34, 0xd4, 0x1f, 0x5c, 0xe7, 0xa5, 0x6f, 0x1d,
-	0x1b, 0xf4, 0xdc, 0xbe, 0x43, 0x06, 0x45, 0x2d, 0x9f, 0xa5, 0x56, 0xc4, 0x86, 0x65, 0xb6, 0xf8,
-	0x2f, 0x52, 0x5b, 0x9d, 0x40, 0x21, 0xe8, 0xa5, 0x25, 0xe7, 0xf1, 0x18, 0xe6, 0xf1, 0xfc, 0xc3,
-	0xb7, 0x83, 0xe9, 0xc5, 0xd7, 0x61, 0x58, 0xbd, 0x66, 0x30, 0x16, 0xe4, 0x1a, 0xa3, 0x1d, 0xe1,
-	0x19, 0x14, 0x11, 0x0d, 0xe4, 0xe9, 0xf2, 0x90, 0xff, 0x68, 0x88, 0x47, 0x37, 0x22, 0xd1, 0xf1,
-	0x1c, 0xc6, 0xb1, 0x74, 0x2c, 0x2f, 0x07, 0x3b, 0xac, 0x7e, 0xf2, 0x97, 0x6f, 0x39, 0xcc, 0xe3,
-	0xf4, 0x96, 0xec, 0xe6, 0x49, 0x11, 0x0a, 0x98, 0x5d, 0x5a, 0x92, 0x9e, 0x52, 0x6c, 0xbf, 0xa8,
-	0x1d, 0x1c, 0x6d, 0xc1, 0x93, 0xb5, 0xea, 0x1f, 0xde, 0xc0, 0xf4, 0x8a, 0x7c, 0x5c, 0x70, 0x5b,
-	0x15, 0x63, 0x7e, 0xbb, 0x28, 0xde, 0x01, 0x7e, 0x29, 0xae, 0xba, 0x3e, 0xd1, 0x3f, 0x10, 0x5e,
-	0x4d, 0xee, 0x53, 0xb0, 0xf5, 0x28, 0x7c, 0xac, 0xd3, 0xf7, 0x00, 0x00, 0x00, 0xff, 0xff, 0x99,
-	0x92, 0xa7, 0x77, 0x69, 0x02, 0x00, 0x00,
+	// 278 bytes of a gzipped FileDescriptorProto
+	0x1f, 0x8b, 0x08, 0x00, 0x00, 0x00, 0x00, 0x00, 0x02, 0xff, 0xac, 0x90, 0x3f, 0x4b, 0x03, 0x41,
+	0x10, 0xc5, 0xbd, 0x24, 0xe6, 0xcc, 0x84, 0x44, 0x18, 0x41, 0x16, 0xb1, 0x38, 0x4e, 0x85, 0x54,
+	0x57, 0x44, 0x50, 0x5b, 0x63, 0x61, 0x67, 0x71, 0x62, 0x93, 0x46, 0xf6, 0x96, 0x41, 0x04, 0xb3,
+	0x7b, 0xee, 0xee, 0x05, 0xae, 0xf6, 0xbb, 0xf8, 0x39, 0xc5, 0xfd, 0x73, 0x22, 0xc8, 0x69, 0x61,
+	0xb7, 0xf3, 0xf8, 0xbd, 0x79, 0xb3, 0x0f, 0x66, 0x5c, 0x08, 0xd5, 0x48, 0x5b, 0xd4, 0x5a, 0x59,
+	0x85, 0x07, 0x1b, 0x25, 0xa9, 0xb5, 0x9a, 0xa8, 0x30, 0x5b, 0xe1, 0xc5, 0xfc, 0x09, 0xd2, 0x6b,
+	0x4f, 0xe1, 0x21, 0x8c, 0x65, 0xb3, 0xa9, 0x48, 0xb3, 0x24, 0x4b, 0x16, 0x93, 0x32, 0x4c, 0x88,
+	0x30, 0xb2, 0x6d, 0x4d, 0x6c, 0xe0, 0x54, 0xf7, 0xfe, 0x64, 0x8d, 0xe5, 0xb6, 0x31, 0x6c, 0xe8,
+	0x59, 0x3f, 0x21, 0x83, 0xb4, 0xe2, 0x2f, 0x5c, 0x0a, 0x62, 0xa3, 0x2c, 0x59, 0xec, 0x96, 0x71,
+	0xcc, 0x2f, 0x61, 0x1e, 0x82, 0x4a, 0x7a, 0x6d, 0xc8, 0x58, 0x3c, 0x83, 0x79, 0x38, 0xf0, 0xf1,
+	0x5b, 0x6e, 0x3c, 0xfb, 0xce, 0x89, 0xf9, 0x5b, 0x02, 0xfb, 0x9d, 0xd3, 0xd4, 0x4a, 0x1a, 0xc2,
+	0x0b, 0x48, 0x03, 0xe4, 0x3c, 0xd3, 0xe5, 0x71, 0xf1, 0xc3, 0xe7, 0x8a, 0x68, 0x8b, 0x30, 0x5e,
+	0xc1, 0x5e, 0x78, 0x1a, 0x36, 0xc8, 0x86, 0xbf, 0x1a, 0x3b, 0x7a, 0xf9, 0x3e, 0xe8, 0xee, 0xbf,
+	0x27, 0xbd, 0x7d, 0x16, 0x84, 0x0f, 0x30, 0xbb, 0xd1, 0xc4, 0x2d, 0xc5, 0x02, 0x7b, 0x77, 0x1d,
+	0x9d, 0xf6, 0x26, 0x85, 0x9f, 0xe5, 0x3b, 0xb8, 0x86, 0xe9, 0x2d, 0xd9, 0xa0, 0x1b, 0x3c, 0xe9,
+	0xb7, 0xb9, 0x2a, 0xff, 0xbc, 0x9b, 0x03, 0x7e, 0xed, 0x5e, 0xb5, 0xbe, 0xe1, 0x7f, 0x8d, 0x58,
+	0x4d, 0xd6, 0xb1, 0xed, 0x6a, 0xec, 0x98, 0xf3, 0x8f, 0x00, 0x00, 0x00, 0xff, 0xff, 0xc1, 0x86,
+	0x78, 0x12, 0x88, 0x02, 0x00, 0x00,
 }
 
 // Reference imports to suppress errors if they are not otherwise used.
@@ -213,9 +213,9 @@ var _ server.Option
 // Client API for AccountService service
 
 type AccountServiceClient interface {
-	CreateAccount(ctx context.Context, in *Account, opts ...client.CallOption) (*Response, error)
-	GetAccounts(ctx context.Context, in *Request, opts ...client.CallOption) (*Response, error)
-	GetAccountByNumber(ctx context.Context, in *Request, opts ...client.CallOption) (*Response, error)
+	CreateAccount(ctx context.Context, in *Account, opts ...client.CallOption) (*AccountResponse, error)
+	GetAccounts(ctx context.Context, in *AccountRequest, opts ...client.CallOption) (*AccountResponse, error)
+	GetAccountByNumber(ctx context.Context, in *AccountRequest, opts ...client.CallOption) (*AccountResponse, error)
 }
 
 type accountServiceClient struct {
@@ -228,7 +228,7 @@ func NewAccountServiceClient(serviceName string, c client.Client) AccountService
 		c = client.NewClient()
 	}
 	if len(serviceName) == 0 {
-		serviceName = "moneytree.svc.account"
+		serviceName = "moneytree.svc.proto"
 	}
 	return &accountServiceClient{
 		c:           c,
@@ -236,9 +236,9 @@ func NewAccountServiceClient(serviceName string, c client.Client) AccountService
 	}
 }
 
-func (c *accountServiceClient) CreateAccount(ctx context.Context, in *Account, opts ...client.CallOption) (*Response, error) {
+func (c *accountServiceClient) CreateAccount(ctx context.Context, in *Account, opts ...client.CallOption) (*AccountResponse, error) {
 	req := c.c.NewRequest(c.serviceName, "AccountService.CreateAccount", in)
-	out := new(Response)
+	out := new(AccountResponse)
 	err := c.c.Call(ctx, req, out, opts...)
 	if err != nil {
 		return nil, err
@@ -246,9 +246,9 @@ func (c *accountServiceClient) CreateAccount(ctx context.Context, in *Account, o
 	return out, nil
 }
 
-func (c *accountServiceClient) GetAccounts(ctx context.Context, in *Request, opts ...client.CallOption) (*Response, error) {
+func (c *accountServiceClient) GetAccounts(ctx context.Context, in *AccountRequest, opts ...client.CallOption) (*AccountResponse, error) {
 	req := c.c.NewRequest(c.serviceName, "AccountService.GetAccounts", in)
-	out := new(Response)
+	out := new(AccountResponse)
 	err := c.c.Call(ctx, req, out, opts...)
 	if err != nil {
 		return nil, err
@@ -256,9 +256,9 @@ func (c *accountServiceClient) GetAccounts(ctx context.Context, in *Request, opt
 	return out, nil
 }
 
-func (c *accountServiceClient) GetAccountByNumber(ctx context.Context, in *Request, opts ...client.CallOption) (*Response, error) {
+func (c *accountServiceClient) GetAccountByNumber(ctx context.Context, in *AccountRequest, opts ...client.CallOption) (*AccountResponse, error) {
 	req := c.c.NewRequest(c.serviceName, "AccountService.GetAccountByNumber", in)
-	out := new(Response)
+	out := new(AccountResponse)
 	err := c.c.Call(ctx, req, out, opts...)
 	if err != nil {
 		return nil, err
@@ -269,9 +269,9 @@ func (c *accountServiceClient) GetAccountByNumber(ctx context.Context, in *Reque
 // Server API for AccountService service
 
 type AccountServiceHandler interface {
-	CreateAccount(context.Context, *Account, *Response) error
-	GetAccounts(context.Context, *Request, *Response) error
-	GetAccountByNumber(context.Context, *Request, *Response) error
+	CreateAccount(context.Context, *Account, *AccountResponse) error
+	GetAccounts(context.Context, *AccountRequest, *AccountResponse) error
+	GetAccountByNumber(context.Context, *AccountRequest, *AccountResponse) error
 }
 
 func RegisterAccountServiceHandler(s server.Server, hdlr AccountServiceHandler, opts ...server.HandlerOption) {
@@ -282,14 +282,14 @@ type AccountService struct {
 	AccountServiceHandler
 }
 
-func (h *AccountService) CreateAccount(ctx context.Context, in *Account, out *Response) error {
+func (h *AccountService) CreateAccount(ctx context.Context, in *Account, out *AccountResponse) error {
 	return h.AccountServiceHandler.CreateAccount(ctx, in, out)
 }
 
-func (h *AccountService) GetAccounts(ctx context.Context, in *Request, out *Response) error {
+func (h *AccountService) GetAccounts(ctx context.Context, in *AccountRequest, out *AccountResponse) error {
 	return h.AccountServiceHandler.GetAccounts(ctx, in, out)
 }
 
-func (h *AccountService) GetAccountByNumber(ctx context.Context, in *Request, out *Response) error {
+func (h *AccountService) GetAccountByNumber(ctx context.Context, in *AccountRequest, out *AccountResponse) error {
 	return h.AccountServiceHandler.GetAccountByNumber(ctx, in, out)
 }
